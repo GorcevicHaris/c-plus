aafe372456e382cc6c3ed79e9dbe741479e878a8
macbook <harisgorcevic1000@gmail.com>
2025-02-27 00:26:48 +0100

    first commit

---
+++ c.cpp
@@ -0,0 +1,38 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+
+//     cout << "Unesite prvi broj: ";
+//     cin >> a;
+
+//     cout << "Unesite drugi broj: ";
+//     cin >> b;
+
+//     int suma = a + b;
+//     int razlika = a - b;
+//     int proizvod = a * b;
+//     int kolicnik = a / b;
+//     int ostatak = a % b;
+
+//     cout << "Suma: " << suma << endl;
+//     cout << "Razlika: " << razlika << endl;
+//     cout << "Proizvod: " << proizvod << endl;
+//     cout << "Kolicnik: " << kolicnik << endl;
+//     cout << "Ostatak: " << ostatak << endl;
+
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Unesite prvi broj";
+    cin >> a;
+    cout << "Unesite drugi broj";
+}
